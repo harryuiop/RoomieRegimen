6ee0d923aa4460e34860e6c065c0af615c9e8ac7
Harrison Ellis <hel46@uclive.ac.nz>
2024-06-24 02:54:34 +1200

    drag and drop implemented

--- src/app/components/RotationTable.tsx
+++ src/app/components/RotationTable.tsx
@@ -4,7 +4,7 @@ import PocketBase from "pocketbase";
 import { DndContext, closestCorners } from "@dnd-kit/core"
 import { useEffect, useState } from "react";
 import { Person } from "person";
-import { SortableContext, verticalListSortingStrategy, useSortable } from "@dnd-kit/sortable"
+import { SortableContext, verticalListSortingStrategy, useSortable, arrayMove } from "@dnd-kit/sortable"
 import NameCard from "./NameCard";
 
 const RotationTable = () => {
@@ -25,8 +25,17 @@ const RotationTable = () => {
         setFlatmates(records.map(record => ({ id: record.id, name: record.name })));
     };
 
-    if (!isClient) {
-        return null; // Render nothing on the server
+    const getFlatmatePos = (id: string) => flatmates.findIndex(flatmates => (flatmates.id === id));
+
+    const handleDragEnd = (event: any) => {
+        const {active, over} = event
+        if (active.id === over.id) return;
+
+        setFlatmates((flatmates) => {
+            const orginalPos = getFlatmatePos(active.id)
+            const newPos = getFlatmatePos(over.id)
+            return arrayMove(flatmates, orginalPos, newPos)
+        })
     }
 
     return (
@@ -41,7 +50,7 @@ const RotationTable = () => {
                 overflowY: 'auto',
             }}
         >
-            <DndContext collisionDetection={closestCorners}>
+            <DndContext onDragEnd={handleDragEnd} collisionDetection={closestCorners}>
                 <SortableContext items={flatmates} strategy={verticalListSortingStrategy}>
                     {flatmates.map((flatmate) => (
                         <div key={flatmate.id}>
